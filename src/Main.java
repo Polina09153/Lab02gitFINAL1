4dd83d4a92a7d65563ea61e83557e2a5864cc7c2
Polina <izashi85@gmail.com>
2023-02-28 22:36:18 +0300

    master 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, " + args[0]);
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        double result = 3 * (Math.pow((Math.cos(x - Math.PI / 6.0)),2)) / (0.5 + Math.sin(y*y));
+        System.out.println(result);
     }
 }
